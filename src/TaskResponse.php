1a9573b41f1bfdf50e5c38206b1ad1419aa38848
luka <leandr1999@gmail.com>
2020-09-01 16:58:25 +0300

    fix: array to object

--- src/TaskResponse.php
+++ src/TaskResponse.php
@@ -12,38 +12,38 @@ class TaskResponse
 
     public function isSuccess(): bool
     {
-        return $this->response['status'] === 'DONE';
+        return $this->response->status === 'DONE';
     }
 
     public function getData(): ?array
     {
-        return $this->response['data'] ?? null;
+        return $this->response->data ?? null;
     }
 
     public function getCreatedAt(): ?int
     {
-        if (!isset($this->response['metadata']['created_at'])) {
+        if (!isset($this->response->metadata->created_at)) {
             return null;
         }
 
-        return (int)($this->response['metadata']['created_at'] / 1000);
+        return (int)($this->response->metadata->created_at / 1000);
     }
 
     public function getRunedAt(): ?int
     {
-        if (!isset($this->response['metadata']['runed_at'])) {
+        if (!isset($this->response->metadata->runed_at)) {
             return null;
         }
 
-        return (int)($this->response['metadata']['runed_at'] / 1000);
+        return (int)($this->response->metadata->runed_at / 1000);
     }
 
     public function getDoneAt(): ?int
     {
-        if (!isset($this->response['metadata']['done_at'])) {
+        if (!isset($this->response->metadata->done_at)) {
             return null;
         }
-        return (int)($this->response['metadata']['done_at'] / 1000);
+        return (int)($this->response->metadata->done_at / 1000);
     }
 
 
